52ed384246295bf9f2683577e81c1508cc443982
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-26 08:59:09 +0300

    no message

--- Settings.php
+++ Settings.php
@@ -1,5 +1,15 @@
 <?php
 // 2017-02-24
 namespace Dfe\KassaCompleet;
+use GingerPayments\Payment\Ginger as G;
 /** @method static Settings s() */
-final class Settings extends \Df\GingerPaymentsBase\Settings {}
+final class Settings extends \Df\GingerPaymentsBase\Settings {
+	/**
+	 * 2017-02-26
+	 * @override
+	 * @see \Df\GingerPaymentsBase\Settings::product()
+	 * @used-by \Df\GingerPaymentsBase\Settings::api()
+	 * @return string
+	 */
+	protected function product() {return G::P_KASSA_COMPLEET;}
+}
