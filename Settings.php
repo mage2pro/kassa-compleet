f3b2e7f640fdee7630be51456ea82a4f6ede35fc
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-26 15:00:34 +0300

    no message

--- Settings.php
+++ Settings.php
@@ -1,9 +1,18 @@
 <?php
 // 2017-02-24
 namespace Dfe\KassaCompleet;
-use GingerPayments\Payment\Ginger as G;
+use GingerPayments\Payment\Order\Transaction\PaymentMethod as M;
 /** @method static Settings s() */
 final class Settings extends \Df\GingerPaymentsBase\Settings {
+	/**
+	 * 2017-02-26
+	 * @override
+	 * @see \GingerPayments\Payment\IProduct::bankTransferCode()
+	 * @used-by \GingerPayments\Payment\Client::createSepaOrder()
+	 * @return string
+	 */
+	function bankTransferCode() {return M::BANK_TRANSFER_ING;}
+
 	/**
 	 * 2017-02-26
 	 * @override
@@ -20,5 +29,5 @@ final class Settings extends \Df\GingerPaymentsBase\Settings {
 	 * @used-by \Df\GingerPaymentsBase\Settings::api()
 	 * @return string
 	 */
-	protected function product() {return G::P_KASSA_COMPLEET;}
+	protected function product() {return 'Kassa Compleet';}
 }
