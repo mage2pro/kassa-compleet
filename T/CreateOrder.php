ac70978f58bde605b212573609ba85d08ddbed05
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-27 12:21:45 +0300

    no message

--- T/CreateOrder.php
+++ T/CreateOrder.php
@@ -1,7 +1,4 @@
 <?php
 namespace Dfe\KassaCompleet\T;
 // 2017-02-27
-final class CreateOrder extends \Df\GingerPaymentsBase\T\CreateOrder {
-	/** 2017-02-27 @test */
-	function t01_ideal() {parent::t01_ideal();}
-}
+final class CreateOrder extends \Df\GingerPaymentsBase\T\CreateOrder {}
