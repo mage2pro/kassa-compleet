447529aba7a765b94343e9130b4c5cb0ab0003ae
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-01 22:28:53 +0300

    no message

--- Source/Option.php
+++ Source/Option.php
@@ -1,8 +1,10 @@
 <?php
 namespace Dfe\KassaCompleet\Source;
+use GingerPayments\Payment\Order\Transaction\PaymentMethod as M;
 // 2017-03-01
+// [Kassa Compleet] Available payment options: https://mage2.pro/t/3248
 /** @method static Option s() */
-class Option extends \Df\GingerPaymentsBase\Source\Option {
+final class Option extends \Df\GingerPaymentsBase\Source\Option {
 	/**
 	 * 2017-03-01
 	 * @override
@@ -10,5 +12,5 @@ class Option extends \Df\GingerPaymentsBase\Source\Option {
 	 * @used-by \Df\GingerPaymentsBase\Source\Option::map()
 	 * @return array(string => string)
 	 */
-	protected function mapExtra() {return [];}
+	protected function mapExtra() {return [M::COD => 'Cash on Delivery'];}
 }
