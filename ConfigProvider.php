baa0a180ab4a17d033467222e64fad3142727f18
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-03 16:46:54 +0300

    0.1.3

--- ConfigProvider.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace Dfe\KassaCompleet;
-// 2017-03-03
-// @used-by https://github.com/mage2pro/kassa-compleet/blob/0.1.2/etc/frontend/di.xml?ts=4#L9
-final class ConfigProvider extends \Df\GingerPaymentsBase\ConfigProvider {}
