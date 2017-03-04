36ddf60acf00bbae3d7ce10d9122b5275f9c9dc6
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-04 09:36:54 +0300

    0.1.5

--- T/GetIdealBanks.php
+++ T/GetIdealBanks.php
@@ -2,4 +2,4 @@
 namespace Dfe\KassaCompleet\T;
 // 2017-02-27
 // «[Kassa Compleet] An example of a response to GET /v1/ideal/issuers/»: https://mage2.pro/t/3386
-final class GetIdealIssuers extends \Df\GingerPaymentsBase\T\GetIdealIssuers {}
+final class GetIdealBanks extends \Df\GingerPaymentsBase\T\GetIdealBanks {}
