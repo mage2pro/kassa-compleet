da92bc3adaf52033135d7d6ca2ce3122f4f2d29f
Dmitrii Fediuk <dfediuk@gmail.com>
2023-02-27 19:45:07 +0300

    1.4.1

--- Source/Option.php
+++ Source/Option.php
@@ -3,12 +3,12 @@ namespace Dfe\KassaCompleet\Source;
 # 2017-03-01
 # [Kassa Compleet] Available payment options: https://mage2.pro/t/3248
 /** @method static Option s() */
-final class Option extends \Df\GingerPaymentsBase\Source\Option {
+final class Option extends \Dfe\GingerPaymentsBase\Source\Option {
 	/**
 	 * 2017-03-01
 	 * @override
-	 * @see \Df\GingerPaymentsBase\Source\Option::mapExtra()
-	 * @used-by \Df\GingerPaymentsBase\Source\Option::map()
+	 * @see \Dfe\GingerPaymentsBase\Source\Option::mapExtra()
+	 * @used-by \Dfe\GingerPaymentsBase\Source\Option::map()
 	 * @return array(string => string)
 	 */
 	protected function mapExtra():array {return ['cash-on-delivery' => 'Cash on Delivery'];}
--- Test/GetIdealBanks.php
+++ Test/GetIdealBanks.php
@@ -2,4 +2,4 @@
 namespace Dfe\KassaCompleet\Test;
 # 2017-02-27
 # «[Kassa Compleet] An example of a response to GET /v1/ideal/issuers/»: https://mage2.pro/t/3386
-final class GetIdealBanks extends \Df\GingerPaymentsBase\Test\GetIdealBanks {}
+final class GetIdealBanks extends \Dfe\GingerPaymentsBase\Test\GetIdealBanks {}
